e786185894a8715c18cc7fe45446b321eb30a271
allansio <142339294+allansio@users.noreply.github.com>
2024-10-02 11:51:43 -0300

    h

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
+// function soma (num1, num2){
+//     return num1+num2;
+   
+//    }
+//    console.log(soma(3,4))
+   
+//    function areaQuadrado (lado){
+//        return lado**2;
+//    }
+//    console.log(areaQuadrado(4))
+   
+//    let media=function(num1,num2,num3){
+//    return num1+num2+num3/3;
+//    let number=parseInt(prompt(media))
+//    }
+   
+   
+   
+   
+//    console.log(media(6,6,8))
 
+let estados=['pr','sc','rs','sp','rj','mg','es','ba','se','pe','al','am','pa','ro','ap','rr','ac','rn','ce','ma','pi','df','go','mt','ms','pb','to']
 
-
-
-
-
-
-function soma (num1, num2){
- return num1+num2;
-
-}
-console.log(soma(3,4))
-
-function areaQuadrado (lado){
-    return lado**2;
-}
-console.log(areaQuadrado(4))
-
-let media=function(num1,num2,num3){
-return num1+num2+num3/3;
-let number=parseInt(prompt(media))
-}
-
-
-
-
-console.log(media(6,6,8))
